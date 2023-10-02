8df99e6b38294335d87c263921e0cf2d6136c15e
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2023-10-02 15:06:29 +0300

    commit

--- my-project/src/Components/Home.js
+++ my-project/src/Components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <h1 className="font-bold uppercase text-5xl text-fuchsia-900 border-b-8 border-blue-600  pt-8 text-center p-3">
+      <h1 className="font-bold uppercase text-5xl bg-pink-500 text-white border-b-8 border-blue-600  pt-6 text-center p-3">
         Welcome Home!
       </h1>
     </>
