5e10c5b529c38aa0c14fe3cfdc45175d8e3b7ce8
Samuel Njoroge <96120597+Sam516-Dev@users.noreply.github.com>
2023-10-10 00:03:29 +0300

    cart component

---
+++ my-project/src/Components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className='text-bold font-bold text-white bg-blue-500 border border-white p-2 px-4 rounded-md'>
+      0 items
+    </div>
+  )
+}
+
+export default Cart
--- my-project/src/Components/Home.js
+++ my-project/src/Components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import Cart from "./Cart";
 const Home = () => {
   return (
     <>
-      <h1 className="font-bold uppercase text-5xl bg-pink-500 text-white border-b-8 border-blue-600  pt-6 text-center p-3">
-        Welcome to our store !
-      </h1>
+      {/* <div className=""> */}
+        <h1 className="font-bold uppercase text-5xl bg-pink-500 text-white border-b-8 border-blue-600  pt-6 text-center p-3">
+          Welcome to our store !
+        </h1>
+        <div className="absolute top-6 right-20">
+          <Cart />
+        </div>
+      {/* </div> */}
     </>
   );
 };
